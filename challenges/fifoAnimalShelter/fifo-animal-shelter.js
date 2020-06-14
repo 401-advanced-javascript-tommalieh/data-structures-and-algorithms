007029174580a3ae2daafb15d31fdc25614a3c41
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-14 17:23:55 +0300

    redid shelter

--- challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,15 +1,27 @@
 'use strict';
 
+class Node{
+  constructor(value){
+    this.value = value;
+  }
+}
+
 class AnimalShelter {
+
   constructor() {
     this.shelter = [];
-    this.first = null;
+    this.front = null;
+    this.back = null;
+
   }
 
   enqueue(animal) {
     if (animal === 'dog' || animal === 'cat') {
-      this.shelter.push(animal);
-      this.first = this.shelter[0];
+      const animalobject = new Node(animal);
+      this.shelter.push(animalobject);
+      this.front = this.shelter[0];
+      this.back = this.shelter[this.shelter.length - 1];
+
     } else {
       return 'Only dogs and cats are allowed';
     }
@@ -18,12 +30,25 @@ class AnimalShelter {
   dequeue(pref) {
     if (pref === 'dog' || pref === 'cat') {
       let releasedAnimal = this.shelter.shift();
-      while (releasedAnimal !== pref) {
+      this.front = this.shelter[0];
+      while (releasedAnimal.value !== pref) {
         releasedAnimal = this.shelter.shift();
+        this.front = this.shelter[0];
       }
-      return releasedAnimal;
+      return releasedAnimal.value;
     } else {
       return null;
     }
   }
 }
+
+let animalshelter = new AnimalShelter();
+console.log(animalshelter),
+animalshelter.enqueue('cat');
+animalshelter.enqueue('dog');
+animalshelter.enqueue('cat');
+animalshelter.enqueue('cat');
+animalshelter.enqueue('dog');
+console.log(animalshelter);
+animalshelter.dequeue('dog');
+console.log(animalshelter);
