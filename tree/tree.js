a3164c0c91a7a4af49ddd8d0bd13298454071bff
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-16 00:31:17 +0300

    done

--- tree/tree.js
+++ tree/tree.js
@@ -44,7 +44,50 @@ class BinaryTree {
         return result;
     }
 
+    breadthFirst(){
+        let result = [];
+        let queue = [];
+        if (this.root !== null){
+            queue.push(this.root);
+            while(queue.length > 0){
+                let node = queue.shift();
+                result.push(node.value);
+                if(node.left !== null){
+                    queue.push(node.left);
+                }
+                if(node.right !== null){
+                    queue.push(node.right);
+                }
+            }
+            return result;
+        }else{
+            return null;
+        }
+    }
+
 }
+
+const one = new Node(1);
+const two = new Node(2);
+const three = new Node(3);
+const four = new Node(4);
+const five = new Node(5);
+const six = new Node(6);
+const seven = new Node(7);
+const eight = new Node(8);
+const nine = new Node(9);
+one.left = two;
+one.right = three;
+three.right = four;
+three.left = five;
+two.left = six;
+six.right = seven;
+seven.left = eight;
+seven.right = nine;
+const tree = new BinaryTree(one);
+console.log(tree);
+const breadthFirst = tree.breadthFirst();
+console.log(breadthFirst);
 
 module.exports = {
     Node,
